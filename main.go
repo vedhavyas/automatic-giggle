39fb8e1e69b63138c25b9dcb28c289db46636a40
vedhavyas singareddi <vedhavyas.singareddi@gmail.com>
2016-04-29 13:11:54 +0530

    Run the http on different routine

--- main.go
+++ main.go
@@ -4,8 +4,19 @@ import (
 	"log"
 	"net/http"
 	"github.com/Instamojo/Instamojo++/httphandlers"
+	"fmt"
+	"os/signal"
+	"os"
+	"syscall"
 )
 
 func main() {
-	go log.Fatal(http.ListenAndServe(":3000", httphandlers.Router))
+	go func() {
+		log.Fatal(http.ListenAndServe(":3000", httphandlers.Router))
+	}()
+	fmt.Println("listening now ...")
+	sigs := make(chan os.Signal)
+	signal.Notify(sigs, syscall.SIGTERM)
+	<-sigs
+	fmt.Println("SIGTERM, time to shutdown")
 }
