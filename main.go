96476430b2825903cae17f804dc668f1a8028817
vedhavyas singareddi <vedhavyas.singareddi@gmail.com>
2016-04-29 13:52:50 +0530

    look for available port

--- main.go
+++ main.go
@@ -4,19 +4,15 @@ import (
 	"log"
 	"net/http"
 	"github.com/Instamojo/Instamojo++/httphandlers"
-	"fmt"
-	"os/signal"
 	"os"
-	"syscall"
 )
 
 func main() {
-	go func() {
-		log.Fatal(http.ListenAndServe(":3000", httphandlers.Router))
-	}()
-	fmt.Println("listening now ...")
-	sigs := make(chan os.Signal)
-	signal.Notify(sigs, syscall.SIGTERM)
-	<-sigs
-	fmt.Println("SIGTERM, time to shutdown")
+
+	port := "3000"
+
+	if os.Getenv("PORT") != ""{
+		port = os.Getenv("PORT")
+	}
+	log.Fatal(http.ListenAndServe(":"+port, httphandlers.Router))
 }
