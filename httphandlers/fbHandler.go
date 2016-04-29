c7b50482046a4ebb49d57696d4a39a490bbd22c4
vedhavyas singareddi <vedhavyas.singareddi@gmail.com>
2016-04-29 15:57:19 +0530

    Add repsonse to fb hook

--- httphandlers/fbHandler.go
+++ httphandlers/fbHandler.go
@@ -5,7 +5,8 @@ import (
 	"fmt"
 	"github.com/Instamojo/Instamojo++/models"
 	"encoding/json"
-	"strings"
+	"bytes"
+	"io/ioutil"
 )
 
 var pageToken = "EAAIOkn4DvqABAAaBaiZAXbxHHzEcYsEfrBBFM0QRAZC8ZCAZChKXMtlcK8wTSegMcJxv03fWRbPlOBF8mz460dB2HcJA8M8Ut8FYZBt0TiZBrZBwVRJZB0T1kxyGSLbj0P9tQsnoR3qwswDx0rlhdJQbQoZAc8WYidLUDyrDYmnFJ9QZDZD"
@@ -22,17 +23,25 @@ func fbHook(w http.ResponseWriter, r *http.Request) {
 	sendMessage(fbModel, "Text received, echo: Hello,"+fbModel.Entry[0].Messaging[0].Message.Text)
 }
 
-func sendMessage(fbModel models.FBModel, text string) {
+func sendMessage(fbModel *models.FBModel, text string) {
 	url := "https://graph.facebook.com/v2.6/me/messages?access_token="+pageToken
 	message := models.MessageModel{Text:text}
-	recipient := models.FBRecipient{ID:fbModel.Entry[0].ID}
+	recipient := models.FBRecipient{ID:string(fbModel.Entry[0].ID)}
 	payload := models.FBPayload{Message:message, Recipient: recipient}
-	req, _ := http.NewRequest("POST", url, json.Marshal(payload))
+	data, _ := json.Marshal(payload)
+	req, _ := http.NewRequest("POST", url, bytes.NewBuffer(data))
 	client := http.Client{}
-	_, err :=client.Do(req)
+	resp, err :=client.Do(req)
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
+
+	defer resp.Body.Close()
+	fmt.Println("response Status:", resp.Status)
+	fmt.Println("response Headers:", resp.Header)
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println("response Body:", string(body))
 }
 
 
